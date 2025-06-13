0b41cf601f16c032801058d09e22dba4a46ef3d7
Gabriel Abramovick <90660427+Bortoliero@users.noreply.github.com>
2025-06-13 19:34:51 -0300

    UPDATE: Códigos Front End Web

---
+++ smart_energy_front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SmartEnergyDashboard from './components/Dashboard';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SmartEnergyDashboard />
+    </div>
+  );
+}
+
+export default App;
